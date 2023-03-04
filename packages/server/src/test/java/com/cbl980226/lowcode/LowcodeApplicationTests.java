7b8cb0aeef14568a5d5856be2f725380d8140666
cbl980226 <binglinchen98@gmail.com>
2023-03-04 23:06:30 +0800

    chore(server): init project

---
+++ packages/server/src/test/java/com/cbl980226/lowcode/LowcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbl980226.lowcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
